884262563f3f08c48b69d2fbae7bf88bf2efc2a6
digicomm <raulw@digicomm.com>
2025-01-17 12:07:36 -0700

    Change to digivue and limit to MSSQL

--- src/Date/Unit.php
+++ src/Date/Unit.php
@@ -4,6 +4,7 @@ namespace digivue\LaravelQueryEnrichMSSQL\Date;
 
 enum Unit: string
 {
+    case MILLISECOND = 'millisecond';
     case SECOND = 'second';
     case MINUTE = 'minute';
     case HOUR = 'hour';
